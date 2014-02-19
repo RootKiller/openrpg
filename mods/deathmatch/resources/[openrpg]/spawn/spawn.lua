86259f0e8205f95e25cb2e932632981eb43b838b
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2014-02-19 18:10:31 +0100

    Updated .gitignore Removed spawn resoruce (now for sure) Removed some trash which I added in previous commit

--- mods/deathmatch/resources/[openrpg]/spawn/spawn.lua
+++
@@ -1,7 +0,0 @@
-function joinHandler() 
-	spawnPlayer(source, 1485, -1738, 13) 
-	fadeCamera(source, true) 
-	setCameraTarget(source, source) 
-	outputChatBox("Witaj na serwerze RPG!", source) 
-end 
-addEventHandler("onPlayerJoin", getRootElement(), joinHandler) 
